fd60779a03c381c23790e5db97b92547d8c67cb6
Francescu Santoni <francescu@stupeflix.com>
2014-04-29 11:38:00 +0200

    Improved Decay test

--- PopPlayground/PPFirstViewController.m
+++ PopPlayground/PPFirstViewController.m
@@ -10,6 +10,7 @@
 #import <POP/POP.h>
 
 @interface PPFirstViewController ()
+@property (strong, nonatomic) IBOutlet UITapGestureRecognizer *tapGesture;
 
 @end
 
@@ -18,23 +19,33 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self performAnimation];
+}
+
+- (IBAction)tapGesturePerformed:(UITapGestureRecognizer *)sender
+{
+    if (sender.state == UIGestureRecognizerStateEnded)
+    {
+        [self performAnimation];
+    }
 }
 
 - (void)performAnimation
 {
+    [self.tapGesture setEnabled:NO];
     // We are just referencing the label via tag, because we're lazy
     CALayer *layer = [(UILabel *)[self.view viewWithTag:10] layer];
     // First let's remove any existing animations
     [layer pop_removeAllAnimations];
     // POPSpringAnimation *anim = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionY];
     POPDecayAnimation  *anim = [POPDecayAnimation animationWithPropertyNamed:kPOPLayerPositionY];
-    //    anim.toValue = @(20.0);
-    anim.fromValue = @(0.0);
+    
+    anim.fromValue = @(150);
+//    anim.toValue = @(200.0);
     anim.velocity = @(self.velocitySlider.value);
     anim.deceleration = self.decelSlider.value;
     anim.completionBlock = ^(POPAnimation *anim, BOOL finished) {
         NSLog(@"Animation has completed.");
+        [self.tapGesture setEnabled:YES];
     };
     [layer pop_addAnimation:anim forKey:@"size"];
 
@@ -50,13 +61,11 @@
     NSLog(@"Velocity value changed. Value is now: %f", sender.value);
     UILabel *label = (UILabel *)[self.view viewWithTag:30];
     label.text = [NSString stringWithFormat:@"Velocity: %f", sender.value];
-    [self performAnimation];
 }
 
 - (IBAction)decelSliderUpdated:(UISlider *)sender {
     NSLog(@"Deceleration value changed. Value is now: %f", sender.value);
     UILabel *label = (UILabel *)[self.view viewWithTag:20];
     label.text = [NSString stringWithFormat:@"Deceleration: %f", sender.value];
-    [self performAnimation];
 }
 @end
